02784d38daeee769e0077d810e656c6881c56ed2
AkashY <akashyadav92133@gmail.com>
2023-07-06 17:15:09 +0530

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -4,32 +4,44 @@ import List from "./component/List";
 
 function page() {
   let senddata = [
-    { fullValue: 300, currValue: 150,name:'coffee' },
-    { fullValue: 300, currValue: 120,name:'pasta' },
+    { fullValue: 300, currValue: 150, name: "coffee" },
+    { fullValue: 300, currValue: 120, name: "pasta" },
   ];
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <div
         className="disp"
         style={{ fontSize: "40px", color: "gray", fontWeight: "bold" }}
-      >Monthly Popular Item
+      >
+        Monthly Popular Item
       </div>
       <div>
-         {
-        senddata.map((itm:any,ind:any)=>{
-         return  (<div className="disp">
-         <div><List ls={itm} /></div> &nbsp;&nbsp;&nbsp; <div><Chart cs={itm} /></div>
-         <br />
-       </div>)
-        })
-      }
-        
+        {senddata.map((itm: any, ind: any) => {
+          return (
+            <div className="disp">
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "90px",
+                  
+                }}
+              >
+                <List ls={itm} />
+              </div>{" "}
+              <div>
+                <Chart cs={itm} />
+              </div>
+              <br />
+            </div>
+          );
+        })}
       </div>
-     
+
       {/* <div className="disp">
         <List ls={listdata} /> &nbsp;&nbsp;&nbsp; <Chart cs={senddata} />
       </div> */}
-      
     </div>
   );
 }
